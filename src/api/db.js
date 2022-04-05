b51bb65239de53a81fe06b08e2582c39faaf469f
martin <martin.lange@csa-germany.de>
2022-04-05 10:01:18 +0200

    change base URL to "api/"

--- src/api/db.js
+++ src/api/db.js
@@ -1,6 +1,6 @@
 const Axios = require("axios");
 
-const baseUrl = "http://localhost:8081";
+const baseUrl = "http://localhost:8081/api";
 const userUrl = `${baseUrl}/usr`;
 const widgetUrl = `${baseUrl}/widget`;
 const screenUrl = `${baseUrl}/screen`;
